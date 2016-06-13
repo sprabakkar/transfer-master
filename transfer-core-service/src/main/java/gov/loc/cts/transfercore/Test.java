73c777fa8c71ac13b9519c13b5d0adefd06529d1
Prabakar Singaram <sprabakkar@gmail.com>
2016-06-13 15:31:31 -0400

    commit

--- transfer-core-service/src/main/java/gov/loc/cts/transfercore/Test.java
+++ transfer-core-service/src/main/java/gov/loc/cts/transfercore/Test.java
@@ -3,7 +3,8 @@ package gov.loc.cts.transfercore;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("dsdsd");
+		System.out.println("dsdsdcscs");
+		System.out.println("dsdsdcscs");
 
 	}
 
